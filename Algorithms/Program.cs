8d49132a22307e3ec6babbd7e5092cb79ca85739
Tanya21269552 <tatyana.talpiyot@gmail.com>
2023-05-15 02:26:14 +0300

    Quick Sort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,46 +1,64 @@
 ﻿using Algorithms;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
-internal class Program
+
+//--------  PRIORITY QUEUE--------
+
+//PriorityQueue_custom pq = new PriorityQueue_custom();
+
+//pq.Encueue("task1", 3);
+//pq.Encueue("task2", 4);
+//pq.Encueue("task3", 1);
+//pq.Encueue("task4", 6);
+//pq.Encueue("task5", 2);
+//pq.Encueue("task6", 1);
+//pq.Print();
+//Console.WriteLine("decueue:");
+
+//pq.Decueue().Print();
+//pq.Decueue().Print();
+
+//pq.Decueue().Print();
+
+
+//pq.Print();
+
+
+
+////C# has a PriorityQueue Class built in:
+
+//PriorityQueue<string, int> cpq = new PriorityQueue<string, int>();
+//cpq.Enqueue("t1", 5);
+//cpq.Enqueue("t2", 3);
+//cpq.Enqueue("t3", 6);
+//cpq.Enqueue("t4", 1);
+//cpq.Enqueue("t5", 2);
+//cpq.Enqueue("t6", 4);
+
+//Console.WriteLine(cpq.Peek());
+//cpq.Dequeue();
+//Console.WriteLine(cpq.Peek());
+
+
+
+
+//--------QUICK SORT--------
+
+
+void printArray(int[] arr)
 {
-    private static void Main(string[] args)
-    {
+
+    string[] sArr = Array.ConvertAll(arr, ( el) => el.ToString());
 
 
-        PriorityQueue_custom pq = new PriorityQueue_custom();
+    Console.WriteLine(string.Join(", ", sArr));
 
-        pq.Encueue("task1", 3);
-        pq.Encueue("task2", 4);
-        pq.Encueue("task3", 1);
-        pq.Encueue("task4", 6);
-        pq.Encueue("task5", 2);
-        pq.Encueue("task6", 1);
-        pq.Print();
-        Console.WriteLine("decueue:");
+};
 
-        pq.Decueue().Print();
-        pq.Decueue().Print();
+int[] arr = { 5, 7, 3, 1, 8, 34, 23, 4, 22 };
+int[] arr2 = { 5, 7, 3 };
 
-        pq.Decueue().Print();
-
-
-        pq.Print();
-
-
-
-        //C# has a PriorityQueue Class built in:
-
-        PriorityQueue<string, int> cpq = new PriorityQueue<string, int>();
-        cpq.Enqueue("t1", 5);
-        cpq.Enqueue("t2", 3);
-        cpq.Enqueue("t3", 6);
-        cpq.Enqueue("t4", 1);
-        cpq.Enqueue("t5", 2);
-        cpq.Enqueue("t6", 4);
-
-        Console.WriteLine(cpq.Peek());
-        cpq.Dequeue();
-        Console.WriteLine(cpq.Peek());
-    }
-}
+printArray(QuickSort.Sort(arr));
---
+++ Algorithms/QuickSort.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+
+
+    internal class QuickSort
+    {
+
+
+        private static int[] Swap(int[] arr, int i1 ,int i2)
+        {
+
+
+            int temp = arr[i1];
+            
+
+            arr[i1] = arr[i2];
+            arr[i2] =temp ;
+
+
+            return arr;
+        
+        
+
+        }
+
+       
+     
+
+        private static int PivotSort(int[]arr ,int left,int right )
+        {
+
+            int i, j, pivot, pivotIndex;
+
+
+          
+
+             i= left - 1;
+                j = right;
+                pivotIndex = right;
+                pivot = arr[pivotIndex];
+
+
+
+
+            while (true)
+            {
+
+                while (arr[++i] < pivot) ;
+                
+                while (arr[--j] > pivot) ;
+
+                if (i > j) break;
+
+                Swap(arr, i, j);
+            }
+            Swap(arr, pivotIndex, i);
+            return i;
+
+        }
+
+
+        public static int[] Sort(int[] arr)
+        {
+
+
+            void quickSort(int[] arr ,int left,int right ) {
+
+                if (right-left<1) return;
+            
+
+                int pivotIndex = PivotSort(arr,left,right);
+
+      
+                quickSort(arr , left , pivotIndex - 1);
+                quickSort(arr , pivotIndex + 1,right);
+
+            }
+
+             quickSort(arr,0,arr.Length-1);
+            return arr;
+        }
+    }
+}
